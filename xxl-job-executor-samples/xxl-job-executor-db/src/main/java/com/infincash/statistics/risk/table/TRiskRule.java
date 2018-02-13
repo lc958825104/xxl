d7c44d16c9f2edffdb56d82fab746051a91506fb
infin_caishuxiao <caishuxiao@infincash.com>
2018-02-13 18:30:41 +0800

    1

---
+++ xxl-job-executor-samples/xxl-job-executor-db/src/main/java/com/infincash/statistics/risk/table/TRiskRule.java
@@ -0,0 +1,11 @@
+package com.infincash.statistics.risk.table;
+
+//FIXME mybatis自动生成模板
+//com.infincash.statistics.risk.table
+public class TRiskRule {
+	String id;
+	String type_code;
+	String name_code;
+	String value;
+	String express;
+}
---
+++ xxl-job-executor-samples/xxl-job-executor-db/src/main/java/com/infincash/statistics/risk/table/TUserRisk.java
@@ -0,0 +1,5 @@
+package com.infincash.statistics.risk.table;
+
+public class TUserRisk {
+
+}
