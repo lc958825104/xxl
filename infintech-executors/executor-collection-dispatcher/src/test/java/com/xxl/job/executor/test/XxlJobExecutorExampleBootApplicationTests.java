2aaeacd8effb384f969f6a54b2815e740bba8543
infin_caishuxiao <caishuxiao@infincash.com>
2018-02-24 15:30:37 +0800

    测试类注释

--- infintech-executors/executor-collection-dispatcher/src/test/java/com/xxl/job/executor/test/XxlJobExecutorExampleBootApplicationTests.java
+++ infintech-executors/executor-collection-dispatcher/src/test/java/com/xxl/job/executor/test/XxlJobExecutorExampleBootApplicationTests.java
@@ -2,23 +2,21 @@ package com.xxl.job.executor.test;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.infincash.cron.collection.CronJobHandler;
 import com.xxl.job.executor.Application;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class ,webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class XxlJobExecutorExampleBootApplicationTests {
 
-	@Autowired
-	CronJobHandler jobHandler;
+//	@Autowired
+//	CronJobHandler jobHandler;
 	
 	@Test
 	public void test() throws Exception {
-		jobHandler.execute("");
+//		jobHandler.execute("");
 	}
 
 }
