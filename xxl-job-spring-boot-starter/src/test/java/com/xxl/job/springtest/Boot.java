442fef2bcdd4a77aa74eaa0d4697cf6051c88fd8
fanhang <shinestmt@163.com>
2021-05-18 14:11:45 +0800

    提供 spring-boot-starter 支持

---
+++ xxl-job-spring-boot-starter/src/test/java/com/xxl/job/springtest/Boot.java
@@ -0,0 +1,13 @@
+package com.xxl.job.springtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot.class, args);
+    }
+
+}
